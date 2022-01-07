a4d8a22b7511873af88ce0119695a43eb9b3581e
v_yyanliao <v_yyanliao@tencent.com>
2022-01-07 14:54:47 +0800

    fix: 优化首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,32 @@
-Page({});
+Page({
+  data: {
+    gameList: [
+      [
+        {
+          path: 'snake',
+          name: '贪吃蛇'
+        },
+        {
+          path: 'fly-bird',
+          name: 'Fly Bird'
+        }
+      ],
+      [
+        {
+          path: '2048',
+          name: '2048'
+        },
+        {
+          path: 'seconds',
+          name: '争分夺秒'
+        }
+      ]
+    ]
+  },
+  toPage(e) {
+    const { currentTarget: { dataset: { path } } } = e
+    wx.navigateTo({
+      url: `/pages/${path}/${path}`,
+    })
+  }
+});
